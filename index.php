3545ab0b0f2fd2c9b1e8c59a8febcdb0bf5c1fb7
prajeet1000 <132644038+prajeet1000@users.noreply.github.com>
2023-06-22 11:16:54 +0530

    Update index.php

--- index.php
+++ index.php
@@ -66,7 +66,7 @@ function test_input($data) {
 
 <h2>PHP Form Validation Example</h2>
 <p><span class="error">* required field</span></p>
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+<!-- <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
   Name: <input type="text" name="name" value="<?php echo $name;?>">
   <span class="error">* <?php echo $nameErr;?></span>
   <br><br>
@@ -86,7 +86,7 @@ function test_input($data) {
   <br><br>
   <input type="submit" name="submit" value="Submit">  
 </form>
-
+ -->
 
   
 
