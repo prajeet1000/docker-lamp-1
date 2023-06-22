53d515e214612759f5d701e73a59a3c77d42e5e6
prajeet1000 <132644038+prajeet1000@users.noreply.github.com>
2023-06-22 11:18:40 +0530

    Update index.php

--- index.php
+++ index.php
@@ -83,10 +83,10 @@ function test_input($data) {
   <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male
   <input type="radio" name="gender" <?php if (isset($gender) && $gender=="other") echo "checked";?> value="other">Other  
   <span class="error">* <?php echo $genderErr;?></span>
-  <br><br>
+  <br><br> -->
   <input type="submit" name="submit" value="Submit">  
 </form>
- -->
+
 
   
 
