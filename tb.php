93e3c51c672ecca52ec5178a29f5d06bd9998b8f
prajeet1000 <132644038+prajeet1000@users.noreply.github.com>
2023-06-22 10:57:47 +0530

    Update tb.php

--- tb.php
+++ tb.php
@@ -13,6 +13,7 @@ $servername = "database-1.chu2hpbhxioh.ap-northeast-1.rds.amazonaws.com";
 id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(30) NOT NULL,
 gender VARCHAR(30) NOT NULL,
+comment VARCHAR(255) NOT NULL,
 reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 )";
 if ($conn->query($sql) === TRUE) {
